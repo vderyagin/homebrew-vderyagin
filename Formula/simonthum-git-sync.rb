5d35918b6ca43a82106ea57cfcc92c497479f444
Victor Deryagin <vderyagin@gmail.com>
2022-09-05 22:36:17 +0300

    does this shit work at all

--- Formula/simonthum-git-sync.rb
+++ Formula/simonthum-git-sync.rb
@@ -1,25 +1,24 @@
-class SimonthumGitSync < Formula
-  revision 1
-  desc 'Safe and simple one-script git synchronization'
-  homepage 'https://github.com/simonthum/git-sync'
-  url 'https://github.com/simonthum/git-sync/archive/refs/heads/master.zip'
-  license 'CC0-1.0'
+# class SimonthumGitSync < Formula
+#   desc 'Safe and simple one-script git synchronization'
+#   homepage 'https://github.com/simonthum/git-sync'
+#   url 'https://github.com/simonthum/git-sync/archive/refs/heads/master.zip'
+#   license 'CC0-1.0'
 
-  depends_on 'git'
+#   depends_on 'git'
 
-  sha256 "6a2fc3988ed087440edac82b6475d53c1b13162369126bdee6196b25f61134ea"
+#   sha256 "6a2fc3988ed087440edac82b6475d53c1b13162369126bdee6196b25f61134ea"
 
-  conflicts_with 'git-extras',
-    because: 'both install a `git-sync` binary'
+#   conflicts_with 'git-extras',
+#     because: 'both install a `git-sync` binary'
 
-  conflicts_with 'git-sync',
-    because: 'both install a `git-sync` binary'
+#   conflicts_with 'git-sync',
+#     because: 'both install a `git-sync` binary'
 
-  def install
-    bin.install 'git-sync'
-  end
+#   def install
+#     bin.install 'git-sync'
+#   end
 
-  test do
-    system 'true'
-  end
-end
+#   test do
+#     system 'true'
+#   end
+# end
