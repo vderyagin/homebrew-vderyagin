093a53b178461edc56047425d892fd3157eb9440
Victor Deryagin <vderyagin@gmail.com>
2022-09-05 22:50:03 +0300

    add gocryptfs formula

---
+++ Formula/gocryptfs.rb
@@ -0,0 +1,21 @@
+class Gocryptfs < Formula
+  desc 'Encrypted overlay filesystem written in Go'
+  homepage 'https://nuetzlich.net/gocryptfs'
+  head 'https://github.com/rfjakob/gocryptfs.git'
+  license 'MIT'
+
+  depends_on cask: 'macfuse'
+
+  depends_on 'go' => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'openssl@1.1'
+
+  def install
+    system './build.bash'
+    bin.install 'gocryptfs'
+  end
+
+  test do
+    system 'true'
+  end
+end
