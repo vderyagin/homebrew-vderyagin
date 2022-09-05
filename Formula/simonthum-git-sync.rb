241fa17360b5a43d99e8f19a1ee5d3d6cceab6fb
Victor Deryagin <vderyagin@gmail.com>
2022-09-05 21:51:45 +0300

    add some sha and dep

--- Formula/simonthum-git-sync.rb
+++ Formula/simonthum-git-sync.rb
@@ -3,8 +3,11 @@ class SimonthumGitSync < Formula
   homepage 'https://github.com/simonthum/git-sync'
   license 'CC0-1.0'
   head 'https://github.com/simonthum/git-sync.git'
+  sha256 'a9927b923e1a9e7a32d040f5e9b3652b7c720eca577158f3c0aad1990803dd5f'
 
   bottle :unneeded
+
+  depends_on 'git'
 
   conflicts_with 'git-extras',
     because: 'both install a `git-sync` binary'
