402b15d5bb88cd00035f9123a923112debcc3bc5
Victor Deryagin <vderyagin@gmail.com>
2022-09-05 21:24:51 +0300

    add a formula

---
+++ Formula/simonthum-git-sync.rb
@@ -0,0 +1,20 @@
+class SimonthumGitSync < Formula
+  desc 'Safe and simple one-script git synchronization'
+  homepage 'https://github.com/simonthum/git-sync'
+  license 'CC0-1.0'
+  head 'https://github.com/simonthum/git-sync.git'
+
+  conflicts_with 'git-extras',
+    because: 'both install a `git-sync` binary'
+
+  conflicts_with 'git-sync',
+    because: 'both install a `git-sync` binary'
+
+  def install
+    bin.install 'git-sync'
+  end
+
+  test do
+    system 'true'
+  end
+end
