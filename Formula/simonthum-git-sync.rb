981a9ab95d821723bdff2a3cc9eaf54288fdccf2
Victor Deryagin <vderyagin@gmail.com>
2022-09-05 22:22:21 +0300

    no head

--- Formula/simonthum-git-sync.rb
+++ Formula/simonthum-git-sync.rb
@@ -2,9 +2,8 @@ class SimonthumGitSync < Formula
   desc 'Safe and simple one-script git synchronization'
   homepage 'https://github.com/simonthum/git-sync'
   license 'CC0-1.0'
-  head 'https://github.com/simonthum/git-sync.git'
-
-  sha256 '6a2fc3988ed087440edac82b6475d53c1b13162369126bdee6196b25f61134ea'
+  #head 'https://github.com/simonthum/git-sync.git'
+  url 'https://github.com/simonthum/git-sync/archive/refs/heads/master.zip'
 
   bottle :unneeded
 
